38005d8655845887b8df14d525dec2c44bff8c2e
albanecoiffe <90681785+albanecoiffe@users.noreply.github.com>
2023-12-08 17:18:54 -0500

    Add files via upload

--- components/ProductDisplayAccess.js
+++ components/ProductDisplayAccess.js
@@ -118,6 +118,84 @@ app.component('product-display', {
                     </div>
                 </div>
             </div>
+            <br><br><br><br>
+
+            <!-- d'ajout accessoires -->
+
+            <div class="product-display container">
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        <img :src="image4" :key="image4" class="img-fluid" alt="product image">
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <h1>{{ title4 }}</h1>
+                        <p>{{ description }}</p>
+                        <p v-if="inStock">In Stock</p>
+                        <p v-else>Out of Stock</p>
+                        <p>Shipping: {{ shipping }}</p>
+                        <ul>
+                            <li v-for="detail in details4" :key="detail">{{ detail }}</li>
+                        </ul>
+                        <div class="color-circle-container">
+                            <div
+                                v-for="(variants4, index) in variants4"
+                                :key="variants4.id"
+                                @mouseover="updateVariant4(index)"
+                                class="color-circle"
+                                :style="{ backgroundColor: variants4.color }">
+                            </div>
+                        </div>
+                        <button
+                            class="button"
+                            :class="{ disabledButton: !inStock }"
+                            :disabled="!inStock"
+                            @click="addToCart4">
+                            Add to Cart
+                        </button>
+
+
+                    </div>
+                </div>
+            </div>
+            <br><br><br><br>
+
+            <!-- d'ajout accessoires -->
+
+            <div class="product-display container">
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        <img :src="image5" :key="image5" class="img-fluid" alt="product image">
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <h1>{{ title5 }}</h1>
+                        <p>{{ description }}</p>
+                        <p v-if="inStock">In Stock</p>
+                        <p v-else>Out of Stock</p>
+                        <p>Shipping: {{ shipping }}</p>
+                        <ul>
+                            <li v-for="detail in details5" :key="detail">{{ detail }}</li>
+                        </ul>
+                        <div class="color-circle-container">
+                            <div
+                                v-for="(variants5, index) in variants5"
+                                :key="variants5.id"
+                                @mouseover="updateVariant5(index)"
+                                class="color-circle"
+                                :style="{ backgroundColor: variants5.color }">
+                            </div>
+                        </div>
+                        <button
+                            class="button"
+                            :class="{ disabledButton: !inStock }"
+                            :disabled="!inStock"
+                            @click="addToCart5">
+                            Add to Cart
+                        </button>
+
+
+                    </div>
+                </div>
+            </div>
 
 
 
@@ -167,7 +245,33 @@ app.component('product-display', {
 
             ],
 
+            product4: 'Hobby horse accessories',
+            brand4: ' EAR BONNETS : ',
+            selectedVariant4: 0,
+            description4: 'You cannot buy Hobby Horse without the associated accessories!',
+            details4: ['Made in France', 'Designed by Romane Briochio', 'Taille : Unique', `Price : 4 CAD`],
 
+            variants4: [
+                { id: 2234, color: '#e57386', image4: 'images/bonnet_rose.webp', quantity4: 1 },
+                { id: 2235, color: '#317AC1', image4: 'images/bonnet_bleu.jpeg', quantity4: 64 },
+                { id: 2235, color: '#F7F6CF', image4: 'images/bonnet_creme.webp', quantity4: 64 },
+
+
+            ],
+
+            product5: 'Hobby horse accessories',
+            brand5: ' Protective Suit Lycra : ',
+            selectedVariant5: 0,
+            description5: 'You cannot buy Hobby Horse without the associated accessories!',
+            details5: ['Made in France', 'Designed by Gregoire Rominville', 'Taille : Unique', `Price : 7 CAD`],
+
+            variants5: [
+                { id: 2234, color: '#e57386', image5: 'images/blanket_rose.webp', quantity5: 1 },
+                { id: 2235, color: '#1E0F1C', image5: 'images/blanket_noir.webp', quantity5: 64 },
+
+
+
+            ],
 
             reviews: []
         }
@@ -200,6 +304,26 @@ app.component('product-display', {
         },
         updateVariant3(index) {
             this.selectedVariant3 = index
+        },
+        addReview(review) {
+            this.reviews.push(review)
+        },
+
+        addToCart4() {
+            this.$emit('add-to-cart', this.variants4[this.selectedVariant4].id) //Communicating Events
+        },
+        updateVariant4(index) {
+            this.selectedVariant4 = index
+        },
+        addReview(review) {
+            this.reviews.push(review)
+        },
+
+        addToCart5() {
+            this.$emit('add-to-cart', this.variants5[this.selectedVariant5].id) //Communicating Events
+        },
+        updateVariant5(index) {
+            this.selectedVariant5 = index
         },
         addReview(review) {
             this.reviews.push(review)
@@ -240,6 +364,26 @@ app.component('product-display', {
         },//Computed Properties
         inStock3() {
             return this.variants3[this.selectedVariant3].quantity3
+        },
+
+        title4() {
+            return this.brand4 + ' ' + this.product4
+        },
+        image4() {
+            return this.variants4[this.selectedVariant4].image4
+        },//Computed Properties
+        inStock4() {
+            return this.variants4[this.selectedVariant4].quantity4
+        },
+
+        title5() {
+            return this.brand5 + ' ' + this.product5
+        },
+        image5() {
+            return this.variants5[this.selectedVariant5].image5
+        },//Computed Properties
+        inStock5() {
+            return this.variants5[this.selectedVariant5].quantity5
         },
     }
 })
